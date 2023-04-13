1650a2758be03a1f55468c4dc550dc8d38bb452c
Stanislav Prokofjev <Prokostas@gmail.com>
2023-04-13 11:39:54 +0300

    first commit

---
+++ vip/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Contact } from './Contact/Contact';
+export { default as Bus } from './Bus/Bus';
+export { default as Hero } from './Hero/Hero';
+export { default as Main } from './Main/Main';
+export { default as Service } from './Service/Service';
